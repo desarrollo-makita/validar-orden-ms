5851980e6b89376ff4dfdcbde364c672022fdc5a
Jorge Herrera <jherrera@makita.cl>
2024-05-31 09:13:29 -0400

    se cambia solicitud a post

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { validarOrden } = require('../controllers/validarOrdenControllers');
 
-router.get('/validar-orden', validarOrden);
+router.post('/validar-orden', validarOrden);
 
 module.exports = router;
